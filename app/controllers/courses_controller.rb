2c76041b5c7edfdf9b4a8f55932262a06628238d
prpdpigithub <pikoit60@gmail.com>
2023-03-06 21:33:33 +0000

    StudentEnrollment completed

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,15 +3,16 @@ class CoursesController < ApplicationController
     matching_courses = Course.all
 
     @list_of_courses = matching_courses.order({ :created_at => :desc })
+    
     render({ :template => "courses/index.html.erb" })
   end
 
   def show
     the_id = params.fetch("path_id")
 
-    matching_courses = Course.where({ :id => the_id })
+    matching_courses = Course.where({ :id => the_id }).at(0)
 
-    @the_course = matching_courses.at(0)
+    @the_course = matching_courses
 
     render({ :template => "courses/show.html.erb" })
   end
